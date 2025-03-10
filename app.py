8b909c16b026252477cbfb61d48e8af14804ff2c
robii1 <robinsalte4@gmail.com>
2025-03-10 17:53:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-# app.py
 from flask import Flask, jsonify
 import recommend_logic
+import os  #os for å hente port fra Render
 
 app = Flask(__name__)
 
-# api-endepunkt for å hente anbefalinger
-@app.route("/recommend_api")
+# API-endepunkt for anbefalinger
+@app.route("/recommend_api", methods=["GET"])
 def recommend_api():
     recommendations = recommend_logic.get_recommendations()
     return jsonify(recommendations)
 
 if __name__ == "__main__":
-    # flask-server lokalt på port 5000
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render sin PORT eller 10000 som fallback
+    app.run(host="0.0.0.0", port=port, debug=True)
